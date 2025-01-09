c2cf515188bf76de5df57af7a4af2f9fc2f9b2db
rayjamil <ryanwekesa019@gmail.com>
2025-01-09 10:06:27 +0300

    Adds multiple files

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+//we target where the hearts are placed
+const hearts = document.querySelectorAll(".like-glyph");
+
+//making our function
+function addLikes (event){
+  const heart = event.target;//target our heart
+    mimicServerCall("url")
+    .then(() => {
+      if( heart.innerText === EMPTY_HEART ) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    }
+    
+    )
+    .catch(error => {
+      const modal = document.getElementById("modal");
+      modal.innerText = "error message!";
+      modal.className = "";
+      setTimeout(() => {
+        modal.className = "hidden";
+      }, 3000);
+    });
+}
+
+for (const glyph of hearts) {
+  glyph.addEventListener("click", addLikes);
+}
 
 
 
